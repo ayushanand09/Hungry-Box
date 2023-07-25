19e3c3cdd0fa0985debe243770171e68e84c0226
Ayush Anand <ayush.anandcallingu@gmail.com>
2023-07-26 01:33:22 +0530

    added fvilers dependency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom"
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
-// import {Provider} from "react-redux"
-// import Store from './Store';
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
